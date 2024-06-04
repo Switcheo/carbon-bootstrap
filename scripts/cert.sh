5ac877d2cc90dc23f94eca377a27ac433ae9dd2a
yan-soon <yan@Yans-MacBook-Pro.local>
2024-06-05 02:29:32 +0800

    add ssl cert generation script

---
+++ scripts/cert.sh
@@ -0,0 +1,33 @@
+mkdir ~/.carbon/config/cert
+
+cd ~/.carbon/config/cert
+
+rm *.pem
+
+# 1. Generate CA's private key and self-signed certificate
+openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
+
+echo "CA's self-signed certificate"
+openssl x509 -in ca-cert.pem -noout -text
+
+# 2. Generate web server's private key and certificate signing request (CSR)
+openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
+
+echo subjectAltName=IP:127.0.0.1 >> server-ext.cnf
+
+# 3. Use CA's private key to sign web server's CSR and get back the signed certificate
+openssl x509 -req -in server-req.pem -days 365 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
+
+echo "Server's signed certificate"
+openssl x509 -in server-cert.pem -noout -text
+
+# 4. Generate client's private key and certificate signing request (CSR)
+openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
+
+echo subjectAltName=IP:127.0.0.1 >> client-ext.cnf
+
+# 5. Use CA's private key to sign client's CSR and get back the signed certificate
+openssl x509 -req -in client-req.pem -days 365 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
+
+echo "Client's signed certificate"
+openssl x509 -in client-cert.pem -noout -text
