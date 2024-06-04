4b5923078e1508265e08faeccbeee63bce21f992
yan-soon <yan@Yans-MacBook-Pro.local>
2024-06-05 03:08:32 +0800

    update v2.43.1 pre-upgrade instructions and cert generation script

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -4,6 +4,9 @@ cd ~/.carbon/config/cert
 
 rm *.pem
 
+VALIDATOR_NODE_IP_ADDRESS=${@:$OPTIND:1}
+ORACLE_SERVICE_NODE_IP_ADDRESS=${@:$OPTIND+1:1}
+
 # 1. Generate CA's private key and self-signed certificate
 openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
 
@@ -13,7 +16,7 @@ openssl x509 -in ca-cert.pem -noout -text
 # 2. Generate web server's private key and certificate signing request (CSR)
 openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
 
-echo subjectAltName=IP:127.0.0.1 >> server-ext.cnf
+echo subjectAltName=IP:${VALIDATOR_NODE_IP_ADDRESS} >> server-ext.cnf
 
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
 openssl x509 -req -in server-req.pem -days 365 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
@@ -24,7 +27,7 @@ openssl x509 -in server-cert.pem -noout -text
 # 4. Generate client's private key and certificate signing request (CSR)
 openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
 
-echo subjectAltName=IP:127.0.0.1 >> client-ext.cnf
+echo subjectAltName=IP:${ORACLE_SERVICE_NODE_IP_ADDRESS} >> client-ext.cnf
 
 # 5. Use CA's private key to sign client's CSR and get back the signed certificate
 openssl x509 -req -in client-req.pem -days 365 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
