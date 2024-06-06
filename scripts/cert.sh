76ffe9769379e6bea9cb5d7f9f9f825e5e01abd5
yan-soon <yan@Yans-MacBook-Pro.local>
2024-06-06 14:41:42 +0800

    change cert validity to 100 yrs

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -11,7 +11,7 @@ cd ${CARBON_HOME_PATH}/config/cert
 rm *.pem
 
 # 1. Generate CA's private key and self-signed certificate
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
+openssl req -x509 -newkey rsa:4096 -days 36500 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
 
 echo "CA's self-signed certificate"
 openssl x509 -in ca-cert.pem -noout -text
@@ -22,7 +22,7 @@ openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -
 echo subjectAltName=IP:${VALIDATOR_NODE_IP_ADDRESS} >> server-ext.cnf
 
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
-openssl x509 -req -in server-req.pem -days 365 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
+openssl x509 -req -in server-req.pem -days 36500 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
 
 echo "Server's signed certificate"
 openssl x509 -in server-cert.pem -noout -text
@@ -33,7 +33,7 @@ openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -
 echo subjectAltName=IP:${ORACLE_SERVICE_NODE_IP_ADDRESS} >> client-ext.cnf
 
 # 5. Use CA's private key to sign client's CSR and get back the signed certificate
-openssl x509 -req -in client-req.pem -days 365 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
+openssl x509 -req -in client-req.pem -days 36500 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
 
 echo "Client's signed certificate"
 openssl x509 -in client-cert.pem -noout -text
