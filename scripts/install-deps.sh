c4d5f23a89de5c3e03afd8f8f8215c47658672f1
holyxiaoxin <jr.loves.maths@gmail.com>
2024-09-11 17:53:42 +0800

    feat: remove cleveldb deps

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -44,45 +44,6 @@ fi
 sudo apt update
 sudo apt-get install jq perl -y
 
-# TODO: remove leveldb deps
-if [ -z "$(ldconfig -p | grep libleveldb.so.1$)" ]; then
-  echo "-- Installing level db"
-
-  sudo apt-get install build-essential cmake -y
-
-  wget https://github.com/google/leveldb/archive/1.23.tar.gz && \
-    tar -zxvf 1.23.tar.gz && \
-    wget https://github.com/google/googletest/archive/release-1.11.0.tar.gz && \
-    tar -zxvf release-1.11.0.tar.gz && \
-    mv googletest-release-1.11.0/* leveldb-1.23/third_party/googletest && \
-
-    wget https://github.com/google/benchmark/archive/v1.5.5.tar.gz && \
-    tar -zxvf v1.5.5.tar.gz && \
-    mv benchmark-1.5.5/* leveldb-1.23/third_party/benchmark && \
-
-    cd leveldb-1.23 && \
-    mkdir -p build && \
-
-    cd build && \
-    cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON .. && \
-    cmake --build . && \
-    sudo cp -P libleveldb.so* /usr/local/lib/ && \
-    sudo ldconfig && \
-    cd .. && \
-
-    sudo cp -r include/leveldb /usr/local/include/ && \
-    cd .. && \
-
-    rm -rf benchmark-1.5.5/ && \
-    rm -f v1.5.5.tar.gz && \
-
-    rm -rf googletest-release-1.11.0/ && \
-    rm -f release-1.11.0.tar.gz && \
-
-    rm -rf leveldb-1.23/ && \
-    rm -f 1.23.tar.gz
-fi
-
 if [ "$SETUP_POSTGRES" = true ] && [ $(dpkg-query -W -f='${Status}' postgresql-13 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
   echo "-- Installing postgresql-13"
 
